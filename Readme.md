1144931a783c6ca4cfef871a177547eaf7cd26be
Alan Cruz <acruz@enki.mx>
2022-11-24 23:25:27 -0500

    Creationgitignore, docs and cleaning project

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# Express Server with GraphQL
+
+## Pre-requisits
+To run server correctly you need to run a docker image from MongoDB.
+The instructions to use docker is [here](https://docs.docker.com/engine/install/). 
+You can run the following commands to pull and run docker image from docker oficial image.
+
+```
+    docker pull mongo:lastest
+    docker run -d -p 27017:27017 --name test-mongo mongo:latest
+
+```
+
+## How to use?
+
+```
+    node run start
+```
+
+## Installation and configuration
+```
+    git clone git@github.com:alancruzblancas/graphql-express.git
+    npm install
+```
--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
-//const graphql = require('graphql')
 const { graphqlHTTP } = require('express-graphql')
 const mongoose = require('mongoose')
-const schema = require('./server/schema/schema')
+const schema = require('./schema/schema')
 const cors = require('cors')
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 4000
+
+const hostMongo = process.env.MONGO_URL || 'localhost'
+const portMongo = process.env.MONGO_PORT || '27017'
+const dataBaseMongo = process.env.MONGO_DATABASE || '27017'
+
+const urlMongoDb = `mongodb://${hostMongo}:${portMongo}/${dataBaseMongo}`
 
 app.use(cors())
 
@@ -15,7 +21,7 @@ app.use('/graphql', graphqlHTTP({
     schema
 }))
 
-mongoose.connect('mongodb://localhost:27017/GraphqlClusterDB',
+mongoose.connect(urlMongoDb,
 )
     .then(() => {
         app.listen(port, () => {
